93a9a6de499d48324f76422efcf6757c27887650
Saujan Tiwari <saujan.tiwari@gmail.com>
2022-04-28 23:10:20 +0545

    food images and data created

---
+++ Task 1 - Create/menuapp/src/foodData.js
@@ -0,0 +1,48 @@
+const data = [
+  {
+    image: "./utils/images/food/burger.jpg",
+    name: "Burger",
+    price: 200,
+  },
+  {
+    image: "./utils/images/food/cake.jpg",
+    name: "Cake",
+    price: 100,
+  },
+  {
+    image: "./utils/images/food/chowmien.jpg",
+    name: "Chowmien",
+    price: 150,
+  },
+  {
+    image: "./utils/images/food/fryrice.jpg",
+    name: "Fried Rice",
+    price: 180,
+  },
+  {
+    image: "./utils/images/food/icecream.jpg",
+    name: "Ice Cream",
+    price: 90,
+  },
+  {
+    image: "./utils/images/food/juice.jpg",
+    name: "Fresh Juice",
+    price: 200,
+  },
+  {
+    image: "./utils/images/food/momo.jpg",
+    name: "Momo",
+    price: 220,
+  },
+  {
+    image: "./utils/images/food/pizza.jpg",
+    name: "Pizza",
+    price: 350,
+  },
+  {
+    image: "./utils/images/food/sandwich.jpg",
+    name: "Sandwich",
+    price: 120,
+  },
+];
+export default data;
