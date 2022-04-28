2e7d937cb06e0d2d02949507818dbce8a76c9093
Saujan Tiwari <saujan.tiwari@gmail.com>
2022-04-28 18:19:51 +0545

    frontend structure created

--- Task 1 - Create/menuapp/src/App.js
+++ Task 1 - Create/menuapp/src/App.js
@@ -1,8 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./pages/landing";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import Dashboard from "./pages/dashboard";
+import Menu from "./pages/menu";
+import Orders from "./pages/orders";
+import NotFound from "./pages/notfound";
+import AppBar from "./components/appbar";
+
 const App = () => {
   return (
-    <center>
-      <h1>Restaurant Menu App</h1>
-    </center>
+    <BrowserRouter>
+      <AppBar></AppBar>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
+
 export default App;
---
+++ Task 1 - Create/menuapp/src/pages/landing.js
@@ -0,0 +1,7 @@
+import "./landing.css";
+
+const Landing = () => {
+  return <section className="section1"></section>;
+};
+
+export default Landing;
