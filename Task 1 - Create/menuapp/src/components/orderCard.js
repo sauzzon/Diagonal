a6ea7846a04fd201fc5022ee188219991081c45b
Saujan Tiwari <saujan.tiwari@gmail.com>
2022-04-29 18:22:44 +0545

    orders frontend created

---
+++ Task 1 - Create/menuapp/src/components/orderCard.js
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import {
+  Grid,
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from "@mui/material";
+import photo from "../utils/images/order.jpg";
+
+const OrderCard = ({ data }) => {
+  const [seeMore, setSeeMore] = useState(false);
+  const getStatusButton = (status) => {
+    if (status === "pending") {
+      return (
+        <Button
+          sx={{ mb: 1, mt: 0 }}
+          fullWidth
+          color="error"
+          variant="contained"
+          size="small"
+        >
+          Pay Now
+        </Button>
+      );
+    } else
+      return (
+        <Button
+          sx={{ mb: 1, mt: 0 }}
+          fullWidth
+          variant="contained"
+          size="small"
+          color="success"
+        >
+          Paid
+        </Button>
+      );
+  };
+  return (
+    <>
+      {data.map((d) => (
+        <Grid item key={d.price} xs={12} sm={6} md={4} lg={4}>
+          <Card>
+            <CardMedia
+              component="img"
+              height="200"
+              image={photo}
+              alt="food picture"
+            />
+            <CardContent>
+              <Typography textAlign="center" variant="h6">
+                Order Date: {d.createdAt}
+              </Typography>
+              <Typography color="red" textAlign="center" variant="h6">
+                Rs. {d.price}
+              </Typography>
+              <div style={{ margin: "20px" }}>
+                {seeMore &&
+                  d.details.map((item) => (
+                    <Typography color="green" textAlign="left" variant="body1">
+                      {item.quantity} {item.name} Rs.{item.price} each
+                    </Typography>
+                  ))}
+              </div>
+
+              <Button
+                fullWidth
+                size="small"
+                onClick={() => {
+                  setSeeMore(!seeMore);
+                }}
+              >
+                See {!seeMore ? "More" : "Less"}
+              </Button>
+            </CardContent>
+            <CardActions>{getStatusButton(d.status)}</CardActions>
+          </Card>
+        </Grid>
+      ))}
+    </>
+  );
+};
+
+export default OrderCard;
--- Task 1 - Create/menuapp/src/pages/orders.js
+++ Task 1 - Create/menuapp/src/pages/orders.js
@@ -1,5 +1,15 @@
+import data from "../ordersData";
+import OrderCard from "../components/orderCard";
+import { Container, Grid } from "@mui/material";
+
 const Orders = () => {
-  return <h1>Orders Page</h1>;
+  return (
+    <Container>
+      <Grid sx={{ mt: 1, mb: 5 }} container spacing={4}>
+        <OrderCard data={data} />
+      </Grid>
+    </Container>
+  );
 };
 
 export default Orders;
