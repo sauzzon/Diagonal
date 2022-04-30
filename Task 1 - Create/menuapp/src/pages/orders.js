0363f604c8c1c782536d6bcda2b9c3fe7c146f3f
Saujan Tiwari <saujan.tiwari@gmail.com>
2022-04-30 19:54:08 +0545

    view order history created

--- Task 1 - Create/menuapp/src/pages/orders.js
+++ Task 1 - Create/menuapp/src/pages/orders.js
@@ -1,15 +1,64 @@
-import data from "../ordersData";
 import OrderCard from "../components/orderCard";
-import { Container, Grid } from "@mui/material";
+import { Container, Grid, CircularProgress } from "@mui/material";
+
+import { useNavigate } from "react-router-dom";
+import { useEffect, useContext, useState } from "react";
+import { Context } from "../context/Context";
+import axios from "axios";
+import Notification from "../components/notification";
 
 const Orders = () => {
+  const [loading, setLoading] = useState(false);
+  const [notification, setNotification] = useState({ type: "", msg: "" });
+  const [orderDetails, setOrderDetails] = useState([]);
+  const { isLoggedIn } = useContext(Context);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      return navigate("/login");
+    } else {
+      setLoading(true);
+      getOrderDetails();
+    }
+  }, [isLoggedIn, navigate]);
+
+  const getOrderDetails = async () => {
+    try {
+      const token = localStorage.getItem("userToken");
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${token}`,
+        },
+      };
+      const orderDetails = await axios.get("/orders", config);
+      setLoading(false);
+      setOrderDetails(orderDetails.data.orders);
+    } catch (error) {
+      setNotification({ type: "error", msg: error.response.data.msg });
+    }
+  };
+
   return (
     <>
-      <Container>
-        <Grid sx={{ mt: 1, mb: 5 }} container spacing={4}>
-          <OrderCard data={data} />
-        </Grid>
-      </Container>
+      {notification.type && (
+        <Notification
+          severity={notification.type}
+          message={notification.msg}
+        ></Notification>
+      )}
+      {loading ? (
+        <CircularProgress
+          sx={{ margin: "auto", position: "absolute", top: "48%", left: "48%" }}
+        />
+      ) : (
+        <Container>
+          <Grid sx={{ mt: 1, mb: 5 }} container spacing={4}>
+            <OrderCard data={orderDetails} />
+          </Grid>
+        </Container>
+      )}
     </>
   );
 };
