7bb9010bd2625ddad812458769de5a4542ade2f1
Saujan Tiwari <saujan.tiwari@gmail.com>
2022-04-30 22:04:58 +0545

    login persisted on page reload

--- Task 1 - Create/menuapp/src/context/Context.js
+++ Task 1 - Create/menuapp/src/context/Context.js
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 export const Context = React.createContext();
 
 export const ContextProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [selectedFood, setSelectedFood] = useState([]);
   const [quantity, setQuantity] = useState([]);
+
+  useEffect(() => {
+    const token = localStorage.getItem("userToken");
+    if (token) setIsLoggedIn(true);
+  });
 
   return (
     <Context.Provider
